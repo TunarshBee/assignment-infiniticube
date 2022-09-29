a07ec27102afab1d14b2d14abe31b7c155418c07
TunarshBee <rafiubello4real@gmail.com>
2022-09-29 23:11:03 +0100

    ..

--- app.js
+++ app.js
@@ -22,8 +22,61 @@ const connection = mysql.createConnection({
 });
 
 app.get("/", (_, res) => {
-  res.send("Go to /users to get the available users");
+  res.send("assignment-infiniticube");
 });
+
+// app.get("/orders", (req, res) => {
+//   let numRows;
+//   let numPerPage = parseInt(req.query.limit, 10) || 1;
+//   let page = parseInt(req.query.page, 10) || 0;
+//   let numPages;
+//   let skip = page * numPerPage;
+//   let limit = skip + numPerPage;
+//   const { user_id } = req.query;
+
+//   client
+//     .query("select count(*) from orders")
+//     .then((results) => {
+//       numRows = results.rows[0].count;
+//       numPages = Math.floor(numRows / numPerPage);
+//       console.log("number of pages:", numPages);
+//     })
+//     .then(() =>
+//       client.query(
+//         `SELECT * from orders where user_id=${user_id} limit ${limit}`
+//       )
+//     )
+//     .then((results) => {
+//       const responsePayload = {
+//         results,
+//       };
+
+//       if (page < numPages) {
+//         responsePayload.pagination = {
+//           current: page,
+//           perPage: numPerPage,
+//           previous: page > 1 ? page - 1 : undefined,
+//           next: page < numPages - 1 ? page + 1 : undefined,
+//         };
+//       } else
+//         responsePayload.pagination = {
+//           err:
+//             "queried page " +
+//             page +
+//             " is >= to maximum page number " +
+//             numPages,
+//         };
+//       res.json({
+//         Total: numRows,
+//         Data: responsePayload.results.rows,
+//       });
+//     })
+//     .catch(function (err) {
+//       console.error(err);
+//       res.json({ err: err });
+//     });
+// });
+
 
 
 app.get("/users", (req, res) => {
